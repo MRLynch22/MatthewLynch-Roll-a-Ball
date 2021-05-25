ab644868fff4f3c994032c07033c9a2287b02d81
Matthew Lynch <s191314@students.aie.edu.au>
2021-05-25 12:28:36 +0930

    Jumping mechanic working

--- MatthewLynch-Roll-a-Ball/Assets/Scripts/BallController.cs
+++ MatthewLynch-Roll-a-Ball/Assets/Scripts/BallController.cs
@@ -6,7 +6,9 @@ public class BallController : MonoBehaviour
 {
 
     public float speed;
+    public float jumpForce;
     private Rigidbody _rigidBody;
+    private bool _isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -17,17 +19,35 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
-            _rigidBody.AddForce(new Vector3(0.0f, 0.2f, 0.0f), ForceMode.Impulse);
+            if (_isGrounded == true)
+            {
+                _rigidBody.AddForce(new Vector3(0.0f, jumpForce, 0.0f), ForceMode.Impulse);
+            }
         }
-
+            
         Vector2 moveVector = Vector2.zero;
         moveVector.x = Input.GetAxis("Horizontal");
         moveVector.y = Input.GetAxis("Vertical");
 
         _rigidBody.AddForce(new Vector3(moveVector.x, 0, moveVector.y) * speed, ForceMode.Force);
     }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            _isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            _isGrounded = false;
+        }
+    }
+
 
     public void DisabledBallController()
     {
